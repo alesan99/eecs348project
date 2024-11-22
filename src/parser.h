fd800a1e60b546949645319a7052bec59ee99067
Tyler Stuckey <tylerwstuckey@gmail.com>
2024-11-22 16:56:11 -0600

    added parser header file

---
+++ src/parser.h
@@ -0,0 +1,30 @@
+class Parser {
+#pragma once
+#include <string>
+#include <vector>
+
+
+public:
+
+    //Data structure which represents single character or operator
+    struct Token {
+        enum Type { Number, Op, Paren } type;
+        std::string value;
+        Token(Type t, std::string v) type(t), value(v) {}
+    }
+
+    struct Node {
+        std::string value;
+            Node* left;
+            Node* right;
+            Node(std::string v) : value(v), left(nullptr), right(nullptr) {}
+            ~Node() {
+                delete left;
+                delete right;
+    }
+
+    //return array of tokens
+    std::vector<Token> tokenize(cost std::string& input);
+    //return pointer to the Node
+    Node* parseTokens(const std::vector<Token>& tokens);
+}
